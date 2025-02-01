150c2f0ff0ab55c57c7b0052e05803fa1ded4039
Mohammed-Abol-Fotouh <moabolfotouh@gmail.com>
2025-02-01 04:02:53 +0200

    Add logic for change grid size after clicking the button

--- script.js
+++ script.js
@@ -1,20 +1,32 @@
 const container = document.querySelector(".container");
-const gridSize = 10;
+let gridSize = 10;
+const btn = document.querySelector("button");
 
-container.style.setProperty("grid-size", gridSize);
-
-for (let i = 0; i < gridSize; i++) {
-	const row = document.createElement("div");
-	row.classList.add("row");
-
-	for (let j = 0; j < gridSize; j++) {
-		const square = document.createElement("div");
-		square.classList.add("square");
-		row.appendChild(square);
-		square.addEventListener("mouseover", () => {
-			square.style.backgroundColor = "red";
-		});
+btn.addEventListener("click", () => {
+	const grid = +prompt("Enter preferred grid size:");
+	if (grid > 100 || grid < 1) {
+		alert("Grid size must be between 1 and 100.");
+		return;
+	} else {
+		gridSize = grid;
 	}
 
-	container.appendChild(row);
-}
+	container.innerHTML = "";
+	document.documentElement.style.setProperty("--grid-size", gridSize);
+
+	for (let i = 0; i < gridSize; i++) {
+		const row = document.createElement("div");
+		row.classList.add("row");
+
+		for (let j = 0; j < gridSize; j++) {
+			const square = document.createElement("div");
+			square.classList.add("square");
+			row.appendChild(square);
+			square.addEventListener("mouseover", () => {
+				square.style.backgroundColor = "red";
+			});
+		}
+
+		container.appendChild(row);
+	}
+});
